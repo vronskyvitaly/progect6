72c57e45d8335161867323d1e937a0598f84706a
Vronsky Vitaly <vronskyvitaly@mail.ru>
2022-11-17 11:24:29 +0300

    Работал с js

--- main.js
+++ main.js
@@ -1,1 +1,15 @@
-let a = 105;
+let obj = {
+	name:"Vitaly",
+	subname:"Vronsky",
+	"Первое свойство": true,
+	greeting: function () {
+		console.log(`Добрый день`);
+	},
+}
+
+
+
+
+
+const obj1 = Object.assign ({},obj)
+
