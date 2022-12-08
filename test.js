0300485b6d56613ecfcd8d87552dc51be7bbbaad
vronskyvitaly <94834175+vronskyvitaly@users.noreply.github.com>
2022-12-08 21:38:07 +0300

    8.12

---
+++ test.js
@@ -0,0 +1,18 @@
+const btnMinys = document.querySelector(`[data="minys"]`)
+console.log(btnMinys)
+const btnPlys = document.querySelector(`[data="plys"]`)
+console.log(btnPlys)
+const btnRes = document.querySelector(`[data="res"]`)
+console.log(btnRes)
+
+console.log()
+
+btnMinys.addEventListener("click", function(){
+
+
+})
+
+btnPlys.addEventListener("click", function(){
+console.log("Нажал на плюс") 
+// res = btnResinnerHTML  
+})
